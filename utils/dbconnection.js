3a7ee34788fef4aa7f15ba59f001b5115700c4a4
soetjm <sofoniyaskediryesuf@gmail.com>
2024-12-08 11:47:17 -0800

    Update dbconnection.js

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('Pharmaciy','postgres','11141994so',{
+const sequelize = new Sequelize('Pharmaciy','postgres','',{
     host:'localhost',
     dialect:'postgres'
 })
